19736dac4713e795355566ce5bea9a610888616f
Emmanuel Omoiya <emmanuelomoiya6@gmail.com>
2024-09-25 08:58:57 +0100

    fix: readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Vue 3 + TypeScript + Vite
-
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
-
-Learn more about the recommended Project Setup and IDE Support in the [Vue Docs TypeScript Guide](https://vuejs.org/guide/typescript/overview.html#project-setup).
+<!-- # Friend -->
+<!-- ![Friend](./public/_static/images/friend-prompt.svg)  -->
+<div style="display: flex; column-gap: 1rem; align-items: center; margin: 3rem; justify-content: center">
+    <img src="./public/_static/images/friend-prompt.svg" />
+<h1 style="font-size: 1.3rem">Friend</h1>
+</div>
--- src/app/page.vue
+++ src/app/page.vue
@@ -2,36 +2,6 @@
 import NavComp from '../components/layouts/nav.vue';
 import FooterComp from '../components/layouts/footer.vue';
 import Hero from '../components/home/hero.vue';
-
-// export default {
-//   name: 'Friend',
-//   metaInfo: {
-//     title: 'Friend',
-//     meta: [
-//       {
-//         name: 'description',
-//         content: 'Introducing Friend. not imaginary.'
-//       },
-//       {
-//         name: 'og:title',
-//         content: 'Friend.'
-//       },
-//       {
-//         name: 'og:description',
-//         content: 'Introducing Friend. not imaginary.'
-//       },
-//       {
-//         name: 'og:type',
-//         content: 'website.'
-//       },
-//     ],
-//     link: [
-//       {
-//         rel: 'canonical', href: 'https://friend.com'
-//       }
-//     ]
-//   }
-// }
 </script>
 
 <template>
